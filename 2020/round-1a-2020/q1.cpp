76176629d685a09c3bedded6c10e495c44b8594b
huxiaoxu2019 <admin@ihuxu.com>
2020-04-17 23:10:47 +0800

    :wrench: r1a 2020 q1

--- 2020/round-1a-2020/q1.cpp
+++ 2020/round-1a-2020/q1.cpp
@@ -1,78 +1,66 @@
-// not solved
+// solved
 #include <bits/stdc++.h>
 using namespace std;
-bool helper(string& data, string& pattern, int sd, int sp) {
-    if (sd < data.size() && sp < pattern.size()) {
-        char d = data[sd], p = pattern[sp];
-        if (p != '*') {
-            if (d == p) return helper(data, pattern, sd + 1, sp + 1);
-            else return false;
-        } else {
-            return helper(data, pattern, sd + 1, sp) || helper(data, pattern, sd, sp + 1);
-        }
-    } else if (sd < data.size() && sp >= pattern.size()) {
-        return false;
-    } else if (sd >= data.size() && sp >= pattern.size()) {
-        return true;
-    } else {
-        for (int i = sp; i < pattern.size(); ++i) {
-            if (pattern[i] != '*') return false;
-        }
-        return true;
-    }
-    return false;
-}
 void solve(int t) {
     int N;
     cin >> N;
-    vector<string> lines(N);
+
+    string pre, suf, mid;
+    vector<string> lines;
+
     for (int i = 0; i < N; ++i) {
         string line;
         cin >> line;
-        lines[i] = line;
+        lines.push_back(line);
     }
-    string pre = "", suf = "";
+
     for (int i = 0; i < N; ++i) {
         auto line = lines[i];
-        string curp = "", curs = "";
-        bool star = false;
-        for (int j = 0; j < line.size(); ++j) {
-            char c = line[j];
-            if (c == '*') {
-                star = true;
-                continue;
-            }
-            if (star) {
-                curs += c;
+        int j = 0;
+        string cpre, csuf, cmid;
+        for (j = 0; j < line.size(); ++j) {
+            if (line[j] == '*') {
+                ++j;
+                break;
             } else {
-                curp += c;
+                cpre += line[j];
             }
         }
-        if (curs.size() > suf.size()) {
-            suf = curs;
+        for (int k = 0; k < min(pre.size(), cpre.size()); ++k) {
+            if (pre[k] != cpre[k]) {
+                cout << "Case #" << t << ": *" << endl;
+                return;
+            }
         }
-        if (curp.size() > pre.size()) {
-            pre = curp;
+        pre = (pre.size() > cpre.size()) ? pre : cpre;
+
+        int x = line.size() - 1;
+        for (; x >= 0; --x) {
+            if (line[x] == '*') {
+                --x;
+                break;
+            } else {
+                csuf = line[x] + csuf;
+            }
         }
-    }
-    //cout << "pre:" <<  pre << "suf: " << suf << endl;
-    bool ans = true;
-    string full = pre + suf;
-    //for (auto line : lines) {
-    for (int i = 0; i < N; ++i) {
-        auto line = lines[i];
-        //cout << "ine:" << line <<" full:" << full <<endl;
-        //cout << "line:" << line << endl;
-        if (helper(full, line, 0, 0) == false) {
-            ans = false;
-            break;
+
+        for (int k = 0; k < min(suf.size(), csuf.size()); ++k) {
+            if (suf[suf.size() - k - 1] != csuf[csuf.size() - k - 1]) {
+                cout << "Case #" << t << ": *" << endl;
+                return;
+            }
         }
+        suf = (suf.size() > csuf.size()) ? suf : csuf;
+
+        for (; j <= x; ++j) {
+            if (line[j] == '*') continue;
+            cmid += line[j];
+        }
+
+        mid += cmid;
     }
-    if (ans) {
-        cout << "Case #" << t << ": " << full << endl;
-    } else {
-        cout << "Case #" << t << ": " << "*";
-    }
+
+    cout << "Case #" << t << ": " << (pre + mid + suf) << endl;
 }
 int main() {
     int T;
